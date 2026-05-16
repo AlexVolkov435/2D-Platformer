7f46fad041d60549fa585f987c20f042b21adfb6
AlexVolkov435 <volkov246@bk.ru>
2026-05-16 16:38:24 +0300

    7

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private float _horizontalOffset;
+    [SerializeField] private float _verticalOffset;
+    
     [SerializeField] private float _speed = 3f; 
     [SerializeField] private Transform _target; 
+    
+    private float _depthOffset = -5f;
     
     private void FixedUpdate() 
     {
         Vector3 position = _target.position;
-        position.z = -5;
-        transform.position = Vector3.Lerp(transform.position, position, _speed * Time.fixedDeltaTime); 
+        
+        Vector3 desiredPosition = new Vector3(
+            position.x + _horizontalOffset,
+            position.y + _verticalOffset,
+            position.z + _depthOffset
+        );
+     
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, _speed * Time.fixedDeltaTime); 
     }
 }
